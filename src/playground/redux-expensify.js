7d52233320d68bd947d835f30420dd21b97611ab
cineron <cineron+gh@gmail.com>
2019-01-28 18:21:09 -0600

    Add spread operator with arrays in Redux

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,28 @@
 import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
+
 //Actions: 
 // ADD_EXPENSE
+// use uuid npm library to generate unique ids
+const addExpense = ({ 
+    description = "", 
+    note = "", 
+    amount = 0, 
+    createdAt = 0
+    } = {}) => ({
+    type: "ADD_EXPENSE",
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        createdAt
+    }
+});
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+    type: "REMOVE_EXPENSE",
+    id
+});
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -13,6 +34,14 @@ import { createStore, combineReducers } from "redux";
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case "ADD_EXPENSE":
+            // return state.concat(action.expense);
+            return [...state, action.expense];
+        
+        case "REMOVE_EXPENSE":
+            return state.filter(({ id }) => {
+                return id !== action.id;
+            });
         default:
             return state;
     }
@@ -38,12 +67,23 @@ const store = createStore(
         expenses: expensesReducer,
         //someOtherValue: someReducer,
         filters: filtersReducer
-
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
+const expenseOne = store.dispatch(addExpense({ 
+    description: "Rent", 
+    amount: 115000 
+}));
+const expenseTwo = store.dispatch(addExpense({ 
+    description: "coffee", 
+    amount: 115 
+}));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 //the final state. What are we building?
 const demoState = {

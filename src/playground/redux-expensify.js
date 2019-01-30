b7e7a223a6c18426c3b99467adb9b1a01a9c8a26
cineron <cineron+gh@gmail.com>
2019-01-29 22:40:01 -0600

    Adds filtering to state

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -122,7 +122,16 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
             return state;
     }
 };
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== "number" || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== "number" || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
+        return startDateMatch && endDateMatch && textMatch;
+    });
+};
 // store creation
 const store = createStore(
     combineReducers({
@@ -133,31 +142,37 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    // console.log(store.getState());
+    console.log(visibleExpenses);
+    
 });
 
-// const expenseOne = store.dispatch(addExpense({ 
-//     description: "Rent", 
-//     amount: 115000 
-// }));
-// const expenseTwo = store.dispatch(addExpense({ 
-//     description: "coffee", 
-//     amount: 115 
-// }));
+const expenseOne = store.dispatch(addExpense({ 
+    description: "Rent", 
+    amount: 115000,
+    createdAt: 1000
+}));
+const expenseTwo = store.dispatch(addExpense({ 
+    description: "coffee", 
+    amount: 115,
+    cratedAt: -1000
+}));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 550} ));
 
-// store.dispatch(setTextFilter("rent"));
+// store.dispatch(setTextFilter("coffee"));
 // store.dispatch(setTextFilter(""));
 
 // store.dispatch(sortByAmount()); //amount
 // store.dispatch(sortByDate()); //date
 
 store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(1250));
 
 //the final state. What are we building?
 const demoState = {

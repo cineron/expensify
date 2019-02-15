ff7c12e5436c305ecc57c6bc2ad544902b1c464a
cineron <cineron+gh@gmail.com>
2019-02-14 21:16:17 -0600

    Sets up enzyme and toJSON and raf/polyfill

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,11 +1,19 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from "enzyme";
+// import ReactShallowRenderer from "react-test-renderer/shallow";
+// import toJSON from "enzyme-to-json"; //added to jest.config.json
 import Header from "../../components/Header";
 
 test("Should render Header correctly", () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    // expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();//toJSON not need because it's run automatically now
+
+    // expect(wrapper.find("h1").text()).toBe("Expensify");
+
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 
     // console.log(renderer.getRenderOutput());
     

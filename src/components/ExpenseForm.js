fdb795164a43c0bcbd63ed4176e633d2cca292c5
cineron <cineron+gh@gmail.com>
2019-02-22 21:31:08 -0600

    Adds tests for user input.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import { SingleDatePicker } from "react-dates";
 
 const now = moment();
-console.log(now.format("MMM Do, YYYY"));
+// console.log(now.format("MMM Do, YYYY"));
 
 class ExpenseForm extends React.Component {
     constructor(props){

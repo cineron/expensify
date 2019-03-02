90a42d9b7145988ec5aaf435fe91da45f8e88b7d
cineron <cineron+github@gmail.com>
2019-03-01 23:43:47 -0600

    Adds changes to webpack to create production version.

--- webpack.config.js
+++ webpack.config.js
@@ -5,35 +5,39 @@
 const path = require("path");
 // console.log(path.join(__dirname, "public"));
 
-module.exports = {
-    mode: "development",
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    // add loader, which is in a module
-    // see webpack.js.org
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
-    },
-    // add source map module
-    devtool: "cheap-module-eval-source-map",
-
-    // set up dev server
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-    }
+//use a function to return an object
+module.exports = (env) => {
+    const isProduction = env === "production";
+    // console.log("env", env);
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        // add loader, which is in a module
+        // see webpack.js.org
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }]
+        },
+        // add source map module
+        devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    
+        // set up dev server
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
